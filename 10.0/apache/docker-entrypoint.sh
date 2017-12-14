b4f308e2d3cf572350aefcf260bc97acfe4699ea
goodrainzh <33080353+goodrainzh@users.noreply.github.com>
2017-12-14 14:06:05 +0800

    Update docker-entrypoint.sh

--- 10.0/apache/docker-entrypoint.sh
+++ 10.0/apache/docker-entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 set -e
 
+[[ $DEBUG ]] && set -x 
+
 if [ ! -e '/var/www/html/version.php' ]; then
 	tar cf - --one-file-system -C /usr/src/owncloud . | tar xf -
 	chown -R www-data /var/www/html
 fi
+
+sleep ${PAUSE:-0}
 
 exec "$@"
