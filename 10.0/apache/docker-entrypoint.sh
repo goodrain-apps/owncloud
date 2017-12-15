7eb24b39430fd90c2bd2e5ee4b56b96b0b35b145
zhouyq <zyq916@gmail.com>
2017-12-15 11:49:58 +0800

    Update docker-entrypoint.sh

--- 10.0/apache/docker-entrypoint.sh
+++ 10.0/apache/docker-entrypoint.sh
@@ -7,11 +7,15 @@ APP_PERSISTENT_DIR="data app config"
 
 for dir in $APP_PERSISTENT_DIR
 do
-    if [ ! -d /${DEFAULT_PERSISTENT_DIR}/${dir} ];then
-        mv /var/www/html/${dir} ${DEFAULT_PERSISTENT_DIR}
-        chown www-data.www-data /${DEFAULT_PERSISTENT_DIR}/${dir} -R
+    if [ ! -d ${DEFAULT_PERSISTENT_DIR}/${dir} ];then
+    
+        [ -d /var/www/html/${dir} ] \
+        && mv /var/www/html/${dir} ${DEFAULT_PERSISTENT_DIR} \
+        || mkdir ${DEFAULT_PERSISTENT_DIR}/${dir}
+
+        chown www-data.www-data ${DEFAULT_PERSISTENT_DIR}/${dir} -R
     else 
-        mv /var/www/html/${dir} /var/www/html/${dir}.bak 
+        [ -d /var/www/html/${dir} ] && mv /var/www/html/${dir} /var/www/html/${dir}.bak 
     fi
     ln -s /${DEFAULT_PERSISTENT_DIR}/${dir} /var/www/html/${dir}
 done
